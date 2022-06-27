68723a64d901688ab70c9bdb2c961afbd829da31
Jennifer <jnmurrin@gmail.com>
2022-06-27 14:20:27 -0500

    style groups

--- templates/groups-card.php
+++ templates/groups-card.php
@@ -28,12 +28,15 @@ $props = wp_json_encode( [
                           :key="group.ID">
                     <div class="group"
                          x-on:click="store.setGroup(group)">
-                        <h3 x-text="group.post_title"></h3>
-                        <!-- TO GET THE LOCATION SHOW UP, YOU NEED TO SET A LOCATION IN THE GROUP SETTINGS -->
-                        <template x-if="group?.location_grid && group.location_grid.length">
-                            <p class="location"
-                               x-text="group.location_grid[0].label"></p>
-                        </template>
+                         <div class="text">
+                            <h3 x-text="group.post_title"></h3>
+                            <!-- TO GET THE LOCATION SHOW UP, YOU NEED TO SET A LOCATION IN THE GROUP SETTINGS -->
+                            <template x-if="group?.location_grid && group.location_grid.length">
+                                <p class="location"
+                                x-text="group.location_grid[0].label"></p>
+                            </template>
+                         </div>
+                        <span class="fi-info"></span>
                     </div>
                 </template>
             </div>
