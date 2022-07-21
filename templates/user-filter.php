faf0b98d7188cba67eb63e774253b59c890dd81f
Craig Wann <craig@codezone.io>
2022-07-21 15:42:17 -0500

    Adding icon to user filter.

--- templates/user-filter.php
+++ templates/user-filter.php
@@ -2,11 +2,16 @@
      x-data="groups_card_leader_filter">
     <div class="typeahead__field">
                     <span class="typeahead__query">
-                        <input x-ref="filter_field"
-                               class="input-height"
-                               name="leader_filter"
-                               placeholder="<?php echo esc_html_x( "Filter by ", 'input field placeholder', 'disciple_tools_groups_card' ) . esc_html($user_label);  ?>"
-                               autocomplete="off">
+                        <div class="typeahead__input-group input-group">
+                          <span class="typeahead__input-group-label input-group-label">
+                            <i class="fi-filter"></i>
+                          </span>
+                              <input x-ref="filter_field"
+                                     class="typeahead__input input-height"
+                                     name="leader_filter"
+                                     placeholder="<?php echo esc_html_x( "Filter by ", 'input field placeholder', 'disciple_tools_groups_card' ) . esc_html( $user_label ); ?>"
+                                     autocomplete="off">
+                        </div>
                      </span>
     </div>
 </div>
